09549fd4973fb2e8af67284df044348d67b31a05
asar17 <aelhaidary2019@gmail.com>
2024-01-05 01:02:59 +0200

    grafbase 6

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -6,25 +6,35 @@ import { graph, config } from '@grafbase/sdk'
 
 const g = graph.Standalone()
 
-const User:any = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
-  githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
-  projects: g.relation(() => Project).list().optional(),
-})
+
 
 // @ts-ignore
-const Project = g.model('Project', {
-  title: g.string().length({ min: 3 }),
+const project = g.type('Project', {
+  title: g.string(),
+  //.length({ min: 3 }),
   description: g.string(), 
   image: g.url(),
   liveSiteUrl: g.url(), 
   githubUrl: g.url(), 
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
+  category: g.string()
+  //.search(),
+  //createdBy: g.relation(() => User),
+})
+
+
+const user = g.type('User', {
+  name: g.string(),
+  //.length({min: 2, max: 100 }),
+  email: g.string(),
+  //.unique(),
+  avatarUrl: g.url(),
+  description: g.string(),
+  //.length({ min: 2, max: 1000 }).optional(),
+  githubUrl: g.url(),
+  //.optional(),
+  linkedinUrl: g.url(),
+  //.optional(), 
+ projects: g.ref(project).list().optional(),
 })
 
 export default config({
