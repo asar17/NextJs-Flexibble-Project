e78493aafc628a46c9c34aeea66b1da08c7082db
asar17 <aelhaidary2019@gmail.com>
2024-01-05 21:23:07 +0200

    api key s

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request";
 import { getUserQuery, createUserMutation } from '../graphql/index'
 const isProduction = process.env.NODE_ENV === 'production'
 const apiURL = isProduction? process.env.NEXT_PUBLIC_GRAFBASE_API_URL  || '': 'http://127.0.0.1:4000/graphql'
-const apiKEY= isProduction? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '': 'letme'
+const apiKEY= isProduction? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '': 'SECRET'
 const serverURL= isProduction? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000'
 const client= new GraphQLClient(apiURL)
 
