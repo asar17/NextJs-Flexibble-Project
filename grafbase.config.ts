bab249e309fffbd9344dbf4fb4ef5fd08fe1ecfb
asar17 <aelhaidary2019@gmail.com>
2024-01-05 05:14:35 +0200

    mon

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -17,27 +17,32 @@ const mongodb = connector.MongoDB('MongoDB', {
 g.datasource(mongodb)
 
 
-// @ts-ignore
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string().length({ min: 2, max: 1000 }).optional(),
-  githubUrl: g.url().optional(),
-  linkedinUrl: g.url().optional(), 
-  projects: g.relation(() => Project).list().optional(),
-})
 
 // @ts-ignore
-const Project = g.model('Project', {
-  title: g.string().length({ min: 3 }),
-  description: g.string(), 
-  image: g.url(),
-  liveSiteUrl: g.url(), 
-  githubUrl: g.url(), 
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
-})
+const project=mongodb.model('Project', {
+      title: g.string().length({ min: 3 }),
+      description: g.string(), 
+      image: g.url(),
+      liveSiteUrl: g.url(), 
+      githubUrl: g.url(), 
+      category: g.string(),
+      //.search(),
+     //createdBy: g.ref(user),
+    }).collection('project')
+
+
+// @ts-ignore
+const user=mongodb.model('User', {
+      name: g.string().length({ min: 2, max: 100 }),
+      email: g.string().unique(),
+      avatarUrl: g.url(),
+      description: g.string().length({ min: 2, max: 1000 }).optional(),
+      githubUrl: g.url().optional(),
+      linkedinUrl: g.url().optional(), 
+      //projects: g.relation(()=>project).list().optional(),
+
+  }) .collection('user')
+
 export default config({
   graph: g,
 
